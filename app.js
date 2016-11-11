a5b424eb2f0212764aef43964934e66a16042b8a
Dmitry Lukashevich <ludn2@mail.ru>
2016-11-11 19:06:38 +0300

    added google and title commands

--- app.js
+++ app.js
@@ -1,102 +1,138 @@
 /*
  * Info bot for VK chats
  * Designed by Dmitry Lukashevich
+ * 
+ * Command list:
+ * -- !google or !g google search query - sends the link to google page 
+ * with your search query results. (implemented)
+ * -- !title of !t text - changes the title of a chat (implemented)
+ * -- !weather city - returns the link to rp5 page containing forecast for 
+ * your city (not implemented)
  */
 "use strict";
 
 const CREDENTIALS = require("./credentials");
 const https = require("https");
 const url = require("url");
+const querystring = require("querystring");
 
+//********** Helpers **********
 const sendRequest = options => {
   const req = https.request(options, res => {
     var fullRes = "";
 
-    res.setEncoding("utf-8");
     res.on("data", data => {
-      fullRes += data;
+      fullRes += data.toString();
     });
 
     res.on("end", () => {
-      JSON.parse(fullRes).response.forEach((val, i) => console.log(i > 0 ? val.body : ""));
+
     });
   });
 
   req.end();
 };
 
-const sendMessage = (chat, message) => {
+const detectLanguage = text => {
+  const RU = ["а","б","в","г","д","е","ё","ж","з","и","й","к","л","м","н","о",
+              "п","р","с","т","у","ф","х","ц","ч","ш","щ","ъ","ы","ь","э","ю",
+              "я"];
+  const cleanText = text.split("").filter(val => isNaN(Number(val)));
+
+  return RU.indexOf(cleanText[0].toLowerCase()) >= 0 ? "ru" : "en";
+};
+//********** API methods **********//
+// message.send request
+const sendMessage = (cmd, message) => {
+  const id = cmd.chat_id === undefined ?
+    "user_id=" + cmd.uid  : "chat_id=" + cmd.chat_id;
+
   sendRequest({
     hostname: "api.vk.com",
-    path: "/method/messages.send?user_id=225947793&" +
-      "chat_id=394315580&message=" + message + 
+    path: "/method/messages.send?" + id + "&" +
+      querystring.stringify({message: message}) +
       "&access_token=" + CREDENTIALS.ACCESS_TOKEN
   });
 };
-
-const isTyping = () => {
+// message.type request
+const isTyping = uid => {
   sendRequest({
     hostname: "api.vk.com",
-    path: "/method/messages.setActivity?user_id=225947793&" +
-      "type=typing" +
+    path: "/method/messages.setActivity?user_id=" + uid +
+      "&type=typing&access_token=" + CREDENTIALS.ACCESS_TOKEN
+  });
+};
+// message.markAsRead request
+const markAsRead = mid => {
+  sendRequest({
+    hostname: "api.vk.com",
+    path: "/method/messages.markAsRead?message_ids=" + mid +
       "&access_token=" + CREDENTIALS.ACCESS_TOKEN
   });
 };
-
+// message.get request
 const getMessages = () => {
   const OPTIONS = {
     hostname: "api.vk.com",
-    path: "/method/messages.get?out=0&=225947793" +
+    path: "/method/messages.get?count=10" +
       "&access_token=" + CREDENTIALS.ACCESS_TOKEN
   };
 
-    const req = https.request(OPTIONS, res => {
+  const req = https.request(OPTIONS, res => {
     var fullRes = "";
 
-    res.setEncoding("utf-8");
     res.on("data", data => {
-      fullRes += data;
+      fullRes += data.toString();
     });
 
     res.on("end", () => {
-      JSON.parse(fullRes).response.forEach((val, i) => {
-        
-        //checkForCommand(i > 0 ? val.body : "!dummy");
-      });
-    });
-  });
+      const msgs = JSON.parse(fullRes).response;
 
+      msgs.filter(val => val.body ? val.read_state === 0 : false).
+        forEach(val => execCmd(val));
+    });
+
+  });
+  
   req.end();
 };
-
-const google = text => {
-  sendMessage(1, "www.google.com/search?q=" + text.replace(" ", "+"));
+// message.editChat request
+const title = (chat, chatTitle) => {
+  sendRequest({
+    hostname: "api.vk.com",
+    path: "/method/messages.editChat?chat_id=" + chat.chat_id + "&" +
+      querystring.stringify({title: chatTitle}) +
+      "&access_token=" + CREDENTIALS.ACCESS_TOKEN
+  });
+};
+//********** Commands **********//
+// !google command
+const google = (cmd, msg) => {
+  const lang = detectLanguage(msg) === "ru" ? "lang_ru" : "lang_en";
+  const URIString = "www.google.com/search?lr=" + lang +
+    "&q=" + msg.split(" ").join("\+");
+  
+  isTyping(cmd.uid);
+  setTimeout(() => {
+    sendMessage(cmd, URIString);
+  }, 2000);
 };
 
-const checkForCommand = (command) => {
-  const COMMAND_LIST = {
-    "!google": google
-  };
+const execCmd = msg => {
+  const COMMAND_LIST = {"!google": google, "!g": google,
+                        "!title": title, "!t": title};
+  const cmd = msg.body.match(/^!\S+/);
+  const cmdText = cmd ? msg.body.slice(cmd[0].length + 1) : null;
 
-  const finalCmd = command.match(/^!\S+/);
-  const cmdText = finalCmd ? command.slice(finalCmd[0].length + 1) : null;
-  const funName = finalCmd ? finalCmd[0].replace("!", "") : null; 
-
-  if (COMMAND_LIST[finalCmd]) {
-    COMMAND_LIST[finalCmd](cmdText);
+  // if command, execute
+  if (COMMAND_LIST[cmd]) {
+    COMMAND_LIST[cmd](msg, cmdText);
   }
+  // mark this message as read so as to not read it twice
+  markAsRead(msg.mid);
 };
 
-//isTyping();
-//setTimeout(sendMessage, 7000);
-const mainLoop = () => {
+(function mainLoop() {
   getMessages();
   setTimeout(mainLoop, 2000);
-};
-
-mainLoop();
-
-
-
-
-
+}());
